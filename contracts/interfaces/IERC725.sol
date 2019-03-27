78d1ac542ff6b85fd6479aab4710ea3b47e304df
Hadrien Croubois <hc@iex.ec>
2019-03-27 14:31:54 +0100

    up iERC725

--- contracts/interfaces/IERC725.sol
+++ contracts/interfaces/IERC725.sol
@@ -6,8 +6,8 @@ interface IERC725
 	event DataChanged(bytes32 indexed key, bytes value);
 	event ContractCreated(address indexed contractAddress);
 
-	function owner  ()                       external view returns (address);
-	function getData(bytes32)                external view returns (bytes memory);
-	function setData(bytes32,bytes calldata) external;
-	// function execute(uint256,address,uint256,bytes calldata) external;
+	function owner  ()                                       external view returns (address);
+	function getData(bytes32)                                external view returns (bytes memory);
+	function setData(bytes32,bytes calldata)                 external;
+	function execute(uint256,address,uint256,bytes calldata) external;
 }

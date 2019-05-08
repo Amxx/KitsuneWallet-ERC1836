7478d0dd1122e1d65885ad9d74b00a729fdf8441
Hadrien Croubois <hc@iex.ec>
2019-05-08 19:01:28 +0200

    0.1.1 & 0.1.2

--- kitsune-sdk/lib/abi.ts
+++ kitsune-sdk/lib/abi.ts
@@ -1,8 +1,8 @@
-import IMaster                        from '@kitsune-wallet/contracts/build/IMaster.json';
-import Proxy                          from '@kitsune-wallet/contracts/build/Proxy.json';
-import WalletOwnable                  from '@kitsune-wallet/contracts/build/WalletOwnable.json';
-import WalletMultisig                 from '@kitsune-wallet/contracts/build/WalletMultisig.json';
-import WalletMultisigRefund           from '@kitsune-wallet/contracts/build/WalletMultisigRefund.json';
-import WalletMultisigRefundOutOfOrder from '@kitsune-wallet/contracts/build/WalletMultisigRefundOutOfOrder.json';
+import IMaster                        from '@kitsune-wallet/contracts/build-minified/IMaster.json';
+import Proxy                          from '@kitsune-wallet/contracts/build-minified/Proxy.json';
+import WalletOwnable                  from '@kitsune-wallet/contracts/build-minified/WalletOwnable.json';
+import WalletMultisig                 from '@kitsune-wallet/contracts/build-minified/WalletMultisig.json';
+import WalletMultisigRefund           from '@kitsune-wallet/contracts/build-minified/WalletMultisigRefund.json';
+import WalletMultisigRefundOutOfOrder from '@kitsune-wallet/contracts/build-minified/WalletMultisigRefundOutOfOrder.json';
 
 export { IMaster, Proxy, WalletOwnable, WalletMultisig, WalletMultisigRefund, WalletMultisigRefundOutOfOrder };

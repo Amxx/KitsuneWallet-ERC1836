780420024e5ca2e68a864d8b00dac2d7f2acc201
Hadrien Croubois <hc@iex.ec>
2019-05-19 14:18:53 +0200

    adding deployments to Ropsten & Goerli

--- kitsune-contracts/tools/deployer.ts
+++ kitsune-contracts/tools/deployer.ts
@@ -9,50 +9,54 @@ ethers.errors.setLogLevel('error');
 import ActiveAdresses from '@kitsune-wallet/contracts/deployments/active.json';
 import WaffleConfig   from '../waffle.json';
 
-
-const provider = ethers.getDefaultProvider('kovan');
+// const provider = ethers.getDefaultProvider('ropsten'); // 3
+const provider = ethers.getDefaultProvider('goerli'); // 5
+// const provider = ethers.getDefaultProvider('kovan'); // 42
 const wallet   = new ethers.Wallet(process.env.MNEMONIC, provider);
 
 
 (async () => {
-	const sdk      = new SDK(provider, wallet);
-	const chainId  = (await provider.getNetwork()).chainId;
 
 	fs.readFile('./flattened.sol', async (err, data) => {
 
-		const deployments = {};
-
+		const sdk      = new SDK(provider, wallet);
+		const chainId  = (await provider.getNetwork()).chainId;
+		const deployed = {};
+		// hash flattened source code
 		const hash = crypto
 			.createHash('sha1')
 			.update(data.toString(), 'utf8')
 			.digest('hex');
+		// compilation options
+		const options =
+		{
+			hash:    hash,
+			solc:    WaffleConfig.solcVersion,
+			options: WaffleConfig.compilerOptions,
+		}
+
+		console.log(`-------------------------------------------------`);
+		console.log(`Chainid: ${chainId}`);
+		console.log(`Source:  ${hash}`);
+		console.log(`-------------------------------------------------`);
 
 		for (let master of [ "WalletOwnable", "WalletMultisig", "WalletMultisigRefund", "WalletMultisigRefundOutOfOrder" ])
 		{
-			var address = null;
-			if (                ActiveAdresses[chainId]                 !== undefined
-				&&                ActiveAdresses[chainId][master]         !== undefined
-				&&                ActiveAdresses[chainId][master].hash     == hash
-				&& JSON.stringify(ActiveAdresses[chainId][master].solc   ) == JSON.stringify(WaffleConfig.solcVersion)
-				&& JSON.stringify(ActiveAdresses[chainId][master].options) == JSON.stringify(WaffleConfig.compilerOptions)
-			)
+			const deployedMaster = (ActiveAdresses[chainId] || {})[master] || {};
+
+			if (JSON.stringify({ hash: deployedMaster.hash, solc: deployedMaster.solc, options: deployedMaster.options }) == JSON.stringify(options))
 			{
-				address = ActiveAdresses[chainId][master].address;
-				console.log(`${master} is already deployed at ${ActiveAdresses[chainId][master].address}`);
+				deployed[master] = deployedMaster;
+				console.log(`${master} is already deployed at ${deployed[master].address}`);
 			}
 			else
 			{
-				address = (await sdk.contracts.deployContract(master, [])).address;
-				console.log(`${master} has been deployed to ${ActiveAdresses[chainId][master].address} (chain ${chainId})`);
-			}
-			deployments[master] = {
-				address: address,
-				hash:    hash,
-				solc:    WaffleConfig.solcVersion,
-				options: WaffleConfig.compilerOptions,
+				const address = (await sdk.contracts.deployContract(master, [])).address;
+				deployed[master] = { address, ...options };
+				console.log(`${master} has been deployed to ${deployed[master].address} (chain ${chainId})`);
 			}
 		}
-		console.log(JSON.stringify(deployments, null, '\t'));
+		console.log(JSON.stringify(deployed, null, '\t'));
 	});
 
 })();

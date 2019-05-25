8b55f2d3208d6e727afbab4fa3a91fef3085c4af
Hadrien Croubois <hc@iex.ec>
2019-05-25 18:32:55 +0200

    lint

--- kitsune-contracts/contracts/masters/ERC721Receiver.sol
+++ kitsune-contracts/contracts/masters/ERC721Receiver.sol
@@ -5,7 +5,11 @@ import "openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol";
 
 contract ERC721Receiver is IERC721Receiver
 {
-	function onERC721Received(address,address,uint256,bytes memory)
+	function onERC721Received(
+		address,
+		address,
+		uint256,
+		bytes memory)
 	public returns (bytes4)
 	{
 		return this.onERC721Received.selector;
